7c2ef55d39a679efbb0972ea721b45ccf4b97a0c
acostacos <acostacarlom@gmail.com>
2025-03-25 02:41:49 +0800

    Add configs and scripts for NUS cluster training

---
+++ hr_train_node_regression.sh
@@ -0,0 +1,27 @@
+python main.py --model 'GCN' --config_path='configs/hr/config_hrp03.yaml' --log_path 'logs/hr/hrp03.log' --seed 42 --debug True
+python main.py --model 'GAT' --config_path='configs/hr/config_hrp03.yaml' --log_path 'logs/hr/hrp03.log' --seed 42
+python main.py --model 'GIN' --config_path='configs/hr/config_hrp03.yaml' --log_path 'logs/hr/hrp03.log' --seed 42
+python main.py --model 'GraphSAGE' --config_path='configs/hr/config_hrp03.yaml' --log_path 'logs/hr/hrp03.log' --seed 42
+python main.py --model 'NodeEdgeGNN' --config_path='configs/hr/config_hrp03.yaml' --log_path 'logs/hr/hrp03.log' --seed 42
+python main.py --model 'SWEGNN' --config_path='configs/hr/config_hrp03.yaml' --log_path 'logs/hr/hrp03.log' --seed 42
+
+python main.py --model 'GCN' --config_path='configs/hr/config_hrp06.yaml' --log_path 'logs/hr/hrp06.log' --seed 42 --debug True
+python main.py --model 'GAT' --config_path='configs/hr/config_hrp06.yaml' --log_path 'logs/hr/hrp06.log' --seed 42
+python main.py --model 'GIN' --config_path='configs/hr/config_hrp06.yaml' --log_path 'logs/hr/hrp06.log' --seed 42
+python main.py --model 'GraphSAGE' --config_path='configs/hr/config_hrp06.yaml' --log_path 'logs/hr/hrp06.log' --seed 42
+python main.py --model 'NodeEdgeGNN' --config_path='configs/hr/config_hrp06.yaml' --log_path 'logs/hr/hrp06.log' --seed 42
+python main.py --model 'SWEGNN' --config_path='configs/hr/config_hrp06.yaml' --log_path 'logs/hr/hrp06.log' --seed 42
+
+python main.py --model 'GCN' --config_path='configs/hr/config_hrp07.yaml' --log_path 'logs/hr/hrp07.log' --seed 42 --debug True
+python main.py --model 'GAT' --config_path='configs/hr/config_hrp07.yaml' --log_path 'logs/hr/hrp07.log' --seed 42
+python main.py --model 'GIN' --config_path='configs/hr/config_hrp07.yaml' --log_path 'logs/hr/hrp07.log' --seed 42
+python main.py --model 'GraphSAGE' --config_path='configs/hr/config_hrp07.yaml' --log_path 'logs/hr/hrp07.log' --seed 42
+python main.py --model 'NodeEdgeGNN' --config_path='configs/hr/config_hrp07.yaml' --log_path 'logs/hr/hrp07.log' --seed 42
+python main.py --model 'SWEGNN' --config_path='configs/hr/config_hrp07.yaml' --log_path 'logs/hr/hrp07.log' --seed 42
+
+python main.py --model 'GCN' --config_path='configs/hr/config_hrp09.yaml' --log_path 'logs/hr/hrp09.log' --seed 42 --debug True
+python main.py --model 'GAT' --config_path='configs/hr/config_hrp09.yaml' --log_path 'logs/hr/hrp09.log' --seed 42
+python main.py --model 'GIN' --config_path='configs/hr/config_hrp09.yaml' --log_path 'logs/hr/hrp09.log' --seed 42
+python main.py --model 'GraphSAGE' --config_path='configs/hr/config_hrp09.yaml' --log_path 'logs/hr/hrp09.log' --seed 42
+python main.py --model 'NodeEdgeGNN' --config_path='configs/hr/config_hrp09.yaml' --log_path 'logs/hr/hrp09.log' --seed 42
+python main.py --model 'SWEGNN' --config_path='configs/hr/config_hrp09.yaml' --log_path 'logs/hr/hrp09.log' --seed 42
--- train_node_regression.sh
+++
@@ -1,6 +0,0 @@
-python main.py --model 'GCN' --log_path 'logs/gcn.log' --seed 42 --debug True
-python main.py --model 'GAT' --log_path 'logs/gat.log' --seed 42
-python main.py --model 'GIN' --log_path 'logs/gin.log' --seed 42
-python main.py --model 'GraphSAGE' --log_path 'logs/graphsage.log' --seed 42
-python main.py --model 'NodeEdgeGNN' --log_path 'logs/nodeedge.log' --seed 42
-python main.py --model 'SWEGNN' --log_path 'logs/swegnn.log' --seed 42
