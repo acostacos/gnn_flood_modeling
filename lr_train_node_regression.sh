765e7527d615c3c0785155d3c6904568ab024a90
acostacos <acostacarlom@gmail.com>
2025-03-27 23:27:00 +0800

    Add slurm parameters for train scripts

--- lr_train_node_regression.sh
+++ lr_train_node_regression.sh
@@ -1,3 +1,9 @@
+#!/bin/sh
+#SBATCH --job-name=lr_preprocess
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --partition=gpu-long
+#SBATCH --gpus=a100-40:1
+
 python main.py --model 'GCN' --config_path='configs/lr_train_config.yaml' --log_path 'logs/lr/lr_gcn.log' --seed 42
 python main.py --model 'GAT' --config_path='configs/lr_train_config.yaml' --log_path 'logs/lr/lr_gat.log' --seed 42
 python main.py --model 'GIN' --config_path='configs/lr_train_config.yaml' --log_path 'logs/lr/lr_gin.log' --seed 42
