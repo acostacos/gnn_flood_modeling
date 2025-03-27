39f7fbceb19cf3c2a74ad04ea48a28cd5304490b
acostacos <acostacarlom@gmail.com>
2025-03-27 23:30:25 +0800

    Change train scripts

--- hr_train_node_regression.sh
+++ hr_train_node_regression.sh
@@ -6,9 +6,9 @@
 #SBATCH --mem-per-cpu=48000
 
 
-srun python main.py --model 'GCN' --config_path='configs/hr_train_config.yaml' --log_path 'logs/hr/hr_gcn.log' --seed 42
-srun python main.py --model 'GAT' --config_path='configs/hr_train_config.yaml' --log_path 'logs/hr/hr_gat.log' --seed 42
-srun python main.py --model 'GIN' --config_path='configs/hr_train_config.yaml' --log_path 'logs/hr/hr_gin.log' --seed 42
-srun python main.py --model 'GraphSAGE' --config_path='configs/hr_train_config.yaml' --log_path 'logs/hr/hr_sage.log' --seed 42
-srun python main.py --model 'NodeEdgeGNN' --config_path='configs/hr_train_config.yaml' --log_path 'logs/hr/hr_nodeedge.log' --seed 42
-srun python main.py --model 'SWEGNN' --config_path='configs/hr_train_config.yaml' --log_path 'logs/hr/hr_swegnn.log' --seed 42
+srun python train.py --model 'GCN' --config_path='configs/hr_train_config.yaml' --log_path 'logs/hr/hr_gcn.log' --seed 42
+srun python train.py --model 'GAT' --config_path='configs/hr_train_config.yaml' --log_path 'logs/hr/hr_gat.log' --seed 42
+srun python train.py --model 'GIN' --config_path='configs/hr_train_config.yaml' --log_path 'logs/hr/hr_gin.log' --seed 42
+srun python train.py --model 'GraphSAGE' --config_path='configs/hr_train_config.yaml' --log_path 'logs/hr/hr_sage.log' --seed 42
+srun python train.py --model 'NodeEdgeGNN' --config_path='configs/hr_train_config.yaml' --log_path 'logs/hr/hr_nodeedge.log' --seed 42
+srun python train.py --model 'SWEGNN' --config_path='configs/hr_train_config.yaml' --log_path 'logs/hr/hr_swegnn.log' --seed 42
--- lr_train_node_regression.sh
+++ lr_train_node_regression.sh
@@ -4,9 +4,9 @@
 #SBATCH --partition=gpu-long
 #SBATCH --gpus=a100-40:1
 
-srun python main.py --model 'GCN' --config_path='configs/lr_train_config.yaml' --log_path 'logs/lr/lr_gcn.log' --seed 42
-srun python main.py --model 'GAT' --config_path='configs/lr_train_config.yaml' --log_path 'logs/lr/lr_gat.log' --seed 42
-srun python main.py --model 'GIN' --config_path='configs/lr_train_config.yaml' --log_path 'logs/lr/lr_gin.log' --seed 42
-srun python main.py --model 'GraphSAGE' --config_path='configs/lr_train_config.yaml' --log_path 'logs/lr/lr_sage.log' --seed 42
-srun python main.py --model 'NodeEdgeGNN' --config_path='configs/lr_train_config.yaml' --log_path 'logs/lr/lr_nodeedge.log' --seed 42
-srun python main.py --model 'SWEGNN' --config_path='configs/lr_train_config.yaml' --log_path 'logs/lr/lr_swegnn.log' --seed 42
+srun python train.py --model 'GCN' --config_path='configs/lr_train_config.yaml' --log_path 'logs/lr/lr_gcn.log' --seed 42
+srun python train.py --model 'GAT' --config_path='configs/lr_train_config.yaml' --log_path 'logs/lr/lr_gat.log' --seed 42
+srun python train.py --model 'GIN' --config_path='configs/lr_train_config.yaml' --log_path 'logs/lr/lr_gin.log' --seed 42
+srun python train.py --model 'GraphSAGE' --config_path='configs/lr_train_config.yaml' --log_path 'logs/lr/lr_sage.log' --seed 42
+srun python train.py --model 'NodeEdgeGNN' --config_path='configs/lr_train_config.yaml' --log_path 'logs/lr/lr_nodeedge.log' --seed 42
+srun python train.py --model 'SWEGNN' --config_path='configs/lr_train_config.yaml' --log_path 'logs/lr/lr_swegnn.log' --seed 42
