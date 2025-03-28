97b3b8b92d851cfd066406b34977817c8c82a821
acostacos <acostacarlom@gmail.com>
2025-03-28 17:17:33 +0800

    Add Dual Regression into script

--- hr_train_node_regression.sh
+++ hr_train_node_regression.sh
@@ -14,4 +14,5 @@ srun python train.py --model 'GAT' --config_path='configs/hr_config.yaml' --log_
 srun python train.py --model 'GIN' --config_path='configs/hr_config.yaml' --log_path 'logs/hr/hr_gin.log' --seed 42
 srun python train.py --model 'GraphSAGE' --config_path='configs/hr_config.yaml' --log_path 'logs/hr/hr_sage.log' --seed 42
 srun python train.py --model 'NodeEdgeGNN' --config_path='configs/hr_config.yaml' --log_path 'logs/hr/hr_nodeedge.log' --seed 42
+srun python train.py --model 'NodeEdgeGNN_Dual' --config_path='configs/hr_config.yaml' --log_path 'logs/hr/hr_nodeedge_dual.log' --seed 42
 srun python train.py --model 'SWEGNN' --config_path='configs/hr_config.yaml' --log_path 'logs/hr/hr_swegnn.log' --seed 42
--- lr_train_node_regression.sh
+++ lr_train_node_regression.sh
@@ -3,7 +3,7 @@
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --partition=gpu-long
 #SBATCH --gpus=a100-40:1
-#SBATCH --mem-per-cpu=64000
+#SBATCH --mem-per-cpu=32000
 
 nvidia-smi
 
@@ -14,4 +14,5 @@ srun python train.py --model 'GAT' --config_path='configs/lr_config.yaml' --log_
 srun python train.py --model 'GIN' --config_path='configs/lr_config.yaml' --log_path 'logs/lr/lr_gin.log' --seed 42
 srun python train.py --model 'GraphSAGE' --config_path='configs/lr_config.yaml' --log_path 'logs/lr/lr_sage.log' --seed 42
 srun python train.py --model 'NodeEdgeGNN' --config_path='configs/lr_config.yaml' --log_path 'logs/lr/lr_nodeedge.log' --seed 42
+srun python train.py --model 'NodeEdgeGNN_Dual' --config_path='configs/lr_config.yaml' --log_path 'logs/lr/lr_nodeedge_dual.log' --seed 42
 srun python train.py --model 'SWEGNN' --config_path='configs/lr_config.yaml' --log_path 'logs/lr/lr_swegnn.log' --seed 42
